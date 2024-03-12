0c5fcf9252548cb8603b7890cc4291a2ada4c248
Aakash <officialaakash97@gmail.com>
2024-03-12 06:58:23 -0400

    Integrate API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ToDo from './components/toDo.jsx';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<ToDo/>}></Route>
+    </Routes>
+  );
+ 
+}
+
+export default App
